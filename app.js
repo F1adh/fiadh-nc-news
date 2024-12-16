5fdafa279da1d1cdc773c5b89597d197487be099
Fiadh <fiadhfinnegan@hotmail.com>
2024-12-16 10:19:08 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { getApi, getTopics, getArticleByID, getAllArticles, getArticleComments, postComment, patchArticle, removeComment, getUsers } = require('./controllers/controllers');
 const app = express();
+const cors = require('cors');
+app.use(cors());
 
 app.use(express.json())
 
